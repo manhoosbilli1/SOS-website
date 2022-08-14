e8278773f22e0c2fffcfcda0c07a3b7dbd9a36f7
Shoaib Mustafa <shoaib.mustafa7@hotmail.com>
2022-08-15 03:58:57 +0500

    succesfully serving html file to node.js server

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./views/index.html', (err,data) => {
+    if(err){
+      console.log(err);
+    }else {
+    // res.write(data);
+    res.end(data);
+    }
+  })
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
